3157772bebe3fd4a2a8a34c5db585d85626db7be
fito <fitob612@gmail.com>
2020-06-12 21:23:08 +0200

    Work with forms. Simple search form.

--- OrderManagement/views.py
+++ OrderManagement/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def search_products(request):
+    return render(request, "search_products.html")
+
+
+def search(request):
+    message = "Searched article: {}".format(request.GET["prod"])
+    return HttpResponse(message)
